005a347af57f32ed10155e05e61193f4318b4584
pandur <tom@undev.de>
2015-12-23 15:17:10 +0100

    form dv pv

--- ajax_formdvpv.php
+++ ajax_formdvpv.php
@@ -5,7 +5,7 @@
 
     //--------------------------------------
     function generate_uuid() {
-        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        return sprintf( 'DVPV%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
             mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
             mt_rand( 0, 0xffff ),
             mt_rand( 0, 0x0fff ) | 0x4000,
@@ -20,8 +20,7 @@
         $jsonArr=Array();
         $jsonArr['errors']=Array();
         $jsonArr['navsteps']=Array();
-
-
+        $jsonArr['errormsg']=Array();
 
         // step 1
 
@@ -52,14 +51,11 @@
             $jsonArr['errors'][]='strassenr';
         }
 
-
-        if($_REQUEST['formdata']['plz']=='')
+        if($_REQUEST['formdata']['plz']=='' || !is_numeric($_REQUEST['formdata']['plz']) || strlen($_REQUEST['formdata']['plz'])!=5 )
         {
             $jsonArr['navsteps'][$currentStep]=false;
             $jsonArr['errors'][]='plz';
         }
-
-
 
         if($_REQUEST['formdata']['vorname']=='' )
         {
@@ -85,13 +81,38 @@
             $jsonArr['errors'][]='email2';
         }
 
-         if($_REQUEST['formdata']['email'] != $_REQUEST['formdata']['email2'])
-         {
-             $jsonArr['errors'][]='email';
-             $jsonArr['errors'][]='email2';
-         }
+        if($_REQUEST['formdata']['email'] =='' && $_REQUEST['formdata']['email2']=='')
+        {
+            $jsonArr['navsteps'][$currentStep]=false;
+            $jsonArr['errors'][]='email';
+            $jsonArr['errors'][]='email2';
+        }
+        else
+        if($_REQUEST['formdata']['email'] != $_REQUEST['formdata']['email2'])
+        {
+            $jsonArr['navsteps'][$currentStep]=false;
+            $jsonArr['errors'][]='email';
+            $jsonArr['errors'][]='email2';
+            $jsonArr['errormsg'][]='email_notsame';
+        }
+        else
+        if (!filter_var($_REQUEST['formdata']['email'], FILTER_VALIDATE_EMAIL))
+        {
+            $jsonArr['navsteps'][$currentStep]=false;
+            $jsonArr['errors'][]='email';
+            $jsonArr['errors'][]='email2';
+            $jsonArr['errormsg'][]='email_invalid';
+        }
 
+        if($_REQUEST['formdata']['ustid']=='' &&  $_REQUEST['formdata']['steuernr']=='')
+        {
+            $jsonArr['navsteps'][$currentStep]=false;
+            $jsonArr['errors'][]='ustid';
+            $jsonArr['errors'][]='steuernr';
+            $jsonArr['errormsg'][]='steuer';
+        }
 
+        /////////////////////////////////////////////////////////////////
          // step 2
          $currentStep=1;
          $jsonArr['navsteps'][$currentStep]=true;
@@ -197,7 +218,6 @@
 
          if($jsonArr['navsteps'][0]==true && $jsonArr['navsteps'][1]==true && $jsonArr['navsteps'][2]==true && $jsonArr['navsteps'][3]==true )
          {
-
              $row=Array();
              $row['id']=generate_uuid();
              $row['json']=json_encode($_REQUEST['formdata']);
@@ -243,17 +263,71 @@
 
         $pdf = new FPDF();
         $pdf->AddPage();
-        $pdf->SetFont('Arial','B',8);
-        $pdf->Cell(0,10, "firma: ".$data->firma);
+        $pdf->SetFont('Arial','B',10);
+
+        $pdf->Cell(0,10, "ID: ".$rows[0]->id,0,1);
+        $pdf->Cell(0,10, "Firma: ".$data->firma,0,1);
+
+
+
+
+        $pdf->Cell(0,6, 'firma: '.$data->firma ,0,1);
+        $pdf->Cell(0,6, 'phone: '.$data->phone ,0,1);
+        $pdf->Cell(0,6, 'strasse: '.$data->strasse ,0,1);
+        $pdf->Cell(0,6, 'strassenr: '.$data->strassenr ,0,1);
+        $pdf->Cell(0,6, 'plz: '.$data->plz ,0,1);
+
+        $pdf->Cell(0,6, 'vorname: '.$data->vorname ,0,1);
+        $pdf->Cell(0,6, 'nachname: '.$data->nachname ,0,1);
+
+        $pdf->Cell(0,6, 'email: '.$data->email ,0,1);
+
+         $pdf->Cell(0,6, 'nennleistung: '.$data->nennleistung ,0,1);
+         $pdf->Cell(0,6, 'zaehlbezeichn: '.$data->zaehlbezeichn ,0,1);
+         $pdf->Cell(0,6, 'registrnr: '.$data->registrnr ,0,1);
+         $pdf->Cell(0,6, 'zaehlernr: '.$data->zaehlernr ,0,1);
+         $pdf->Cell(0,6, 'eigenverbrauch: '.$data->eigenverbrauch ,0,1);
+         $pdf->Cell(0,6, 'anlage_strasse: '.$data->anlage_strasse ,0,1);
+         $pdf->Cell(0,6, 'anlage_strassenr: '.$data->anlage_strassenr ,0,1);
+         $pdf->Cell(0,6, 'anlage_plz: '.$data->anlage_plz ,0,1);
+
+         $pdf->Cell(0,6, 'anlage_ort: '.$data->anlage_ort ,0,1);
+         $pdf->Cell(0,6, 'netzbetreiber: '.$data->netzbetreiber ,0,1);
+         $pdf->Cell(0,6, 'konto_inhaber: '.$data->konto_inhaber ,0,1);
+         $pdf->Cell(0,6, 'konto_iban: '.$data->konto_iban ,0,1);
+         $pdf->Cell(0,6, 'konto_bic: '.$data->konto_bic ,0,1);
+
+         $pdf->Cell(0,6, 'konto_nr: '.$data->konto_nr ,0,1);
+         $pdf->Cell(0,6, 'konto_blz: '.$data->konto_blz ,0,1);
+         $pdf->Cell(0,6, 'konto_institut: '.$data->konto_institut ,0,1);
+
+
+
+
+
+
+
+
+
+
         $pdf->Output();
-        //
         // $pdf->Output('../','F');
 
         die();
     }
 
+    function ajax_formDvPv_zip()
+    {
+        global $wpdb;
+        $rows = $wpdb->get_results('SELECT * FROM next_zip WHERE zip="'.esc_sql($_REQUEST['zip']).'";');
+
+        echo json_encode($rows[0]);
+        die();
+    }
+
     add_action( 'wp_ajax_formdvpv', 'ajax_formDvPv' );
     add_action( 'wp_ajax_formdvpv_pdf', 'ajax_formDvPv_pdf' );
+    add_action( 'wp_ajax_formdvpv_zip', 'ajax_formDvPv_zip' );
 
 
 
