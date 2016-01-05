93a7508de140daf1d5dfb7fc5146eda2c2cce746
pandur <tom@undev.de>
2016-01-05 18:25:27 +0100

    gallery

---
+++ ajax_gallery.php
@@ -0,0 +1,36 @@
+<?php
+
+
+    add_action( 'wp_ajax_nopriv_gallery', 'ajax_gallery' );
+    add_action( 'wp_ajax_gallery', 'ajax_gallery' );
+
+
+    function ajax_gallery()
+    {
+        $cat=335;
+
+        $query_images_args = array(
+            'post_type' => 'attachment', 'post_mime_type' =>'image', 'post_status' => 'inherit', 'posts_per_page' => -1,
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'tax_query' => array( array('taxonomy' => 'media_category', 'field' => 'id', 'terms' => $cat ) )
+        );
+
+        $query_images = new WP_Query( $query_images_args );
+
+        $images = array();
+        foreach ( $query_images->posts as $image)
+        {
+            $images[]=array(
+                'url'=> wp_get_attachment_url( $image->ID ),
+                'descr'=>$image->post_excerpt );
+        }
+
+        echo json_encode($images);
+        // var_dump($images);
+        die();
+    }
+
+
+
+?>
---
+++ remit.php
@@ -0,0 +1,6 @@
+<?php
+
+    $jsonFileName = str_replace('php','json', __FILE__ );
+    include("unlist/unlist.php");
+
+?>
